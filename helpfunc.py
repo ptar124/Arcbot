e5f53b54a5bbf4521d3b24dc7a2845ae048d7b49
ptar124 <party48235@ymail.com>
2021-09-15 08:36:26 +0000

    minor bug fixes

--- helpfunc.py
+++ helpfunc.py
@@ -10,9 +10,11 @@ async def helpfunc(ctx, arg):
         await ctx.channel.send("``hard [shiny] [pure] [far] [lost]`` - A harder judge for shiny acc battling, uses RAVON's ratio of 5:3:1:0")
     elif arg == "exhard":
         await ctx.channel.send("``exhard [shiny] [pure] [far] [lost]`` - An even harder judge for shiny acc battling, pures are worth half of shinies, fars and below don't count")
+    elif arg == "chuni":
+        await ctx.channel.send("``chuni [shiny] [pure] [far] [lost]`` - An alternative scoring system based on CHUNITHM")
     elif arg == "pack":
         await ctx.channel.send("``all`` - All Songs\n``free`` - All Free Songs (Arcaea + World Extend)\n``ma`` - Memory Archive\n``arc`` - Arcaea\n``we`` - World Extend\n``bf`` - Black Fate\n``ap`` - Adverse Prelude\n``ls`` - Luminous Sky\n``vl`` - Vicious Labyrinth\n``ec`` - Eternal Core\n``sr`` - Sunset Radiance\n``ar`` - Absolute Reason\n``be`` - Binary Enfold\n``av`` - Ambivalent Vision\n``cs`` - Crimson Solace\n``ep`` - Ephemeral Page\n``tjo`` - The Journey Onwards\n``eo`` - Esoteric Order\n``pt`` - Pale Tapestry\n``los`` - Light of Salvation\n``dh`` - Divided Heart\n``mai`` - maimai\n``ong`` - O.N.G.E.K.I\n``chu`` - CHUNITHM\n``chu2`` - CHUNITHM Collab 2nd Chapter\n``gc`` - Groove Coaster\n``lan`` - Lanota\n``dnx`` - Dynamix\n``wac`` - WACCA\n``cus`` - Custom List")
     else:
-        helpstr = "Current Arcbot Commands:```\nrand\ncuslist\ncombine\nhard\nexhard```"
+        helpstr = "Current Arcbot Commands:```\n--------song list commands--------\nrand\ncuslist\ncombine\n\n--------alternative judge commands--------\nhard\nexhard\nchuni```"
         helpstr = helpstr + "For more information on each command, type ``$help [command]`` or for pack abbrieviations, type ``$help pack``"
         await ctx.channel.send(helpstr)
